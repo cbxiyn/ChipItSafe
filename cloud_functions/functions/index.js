d1cf9021158d95e0f2dbdf77500382112a68dbbd
Christopher Bailey <c.bailey@reply.eu>
2018-10-08 15:06:38 +0100

    Create User Configuration function created

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -1,5 +1,3 @@
-const functions = require('firebase-functions');
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,38 +5,64 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
+//Application Name for email
+const APP_NAME = 'Chip-It-Safe: ECG First Aid';
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
-
 
 //https://firebase.google.com/docs/reference/functions/functions.auth.UserRecord
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.createUserConfig = functions.auth.user().onCreate((user) => {
-    // [END onCreateTrigger]
-      // [START eventAttributes]
-      const uid = user.uid;
-      const email = user.email; // The email of the user.
-      const displayName = user.displayName; // The display name of the user.
-      // [END eventAttributes]
-    
-      return sendWelcomeEmail(email, displayName);
-    });
-    function createUserConfig()
-{}
-    async function sendWelcomeEmail(email, displayName) {
-        const mailOptions = {
-          from: `${APP_NAME} <noreply@firebase.com>`,
-          to: email,
-        };
-      
-        // The user subscribed to the newsletter.
-        mailOptions.subject = `Welcome to ${APP_NAME}!`;
-        mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-        await mailTransport.sendMail(mailOptions);
-        return console.log('New welcome email sent to:', email);
-      }
+
+/**
+ * Trigger definitions
+ */
+/*exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+  // [END onCreateTrigger]
+    // [START eventAttributes]
+    const email = user.email; // The email of the user.
+    const displayName = user.displayName; // The display name of the user.
+    // [END eventAttributes]
+  
+    return sendWelcomeEmail(email, displayName);
+  });*/
+
+exports.createUserConfig = functions.auth.user().onCreate(user => {
+  // [END onCreateTrigger]
+  // [START eventAttributes]
+  var data = {
+    uid: user.uid,
+    email: user.email,
+    displayName: user.displayName,
+    phoneNumber: user.phoneNumber,
+    emailVerified: user.emailVerified
+  }
+  // [END eventAttributes]
+
+  createUserConfig(data);
+});
+
+/**
+ * Create user configuration for user in user config DB
+ */
+function createUserConfig(data) {
+    var setDoc =  db.collection('users').doc(data.uid).set(data);
+}
+/**
+ * Sends a welcome email to new user.
+ 
+async function sendWelcomeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email
+  };
+
+  // The user subscribed to the newsletter.
+  mailOptions.subject = `Welcome to ${APP_NAME}!`;
+  mailOptions.text = `Hey ${displayName ||
+    ""}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
+  await mailTransport.sendMail(mailOptions);
+  return console.log("New welcome email sent to:", email);
+}*/
