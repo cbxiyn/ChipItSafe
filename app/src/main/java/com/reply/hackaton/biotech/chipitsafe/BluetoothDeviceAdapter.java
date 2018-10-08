51af10c61cf13777d431310350a5a6deed683c0f
Mario Salierno <m.salierno@reply.it>
2018-10-08 19:24:36 +0200

    adapter removed comments

--- app/src/main/java/com/reply/hackaton/biotech/chipitsafe/BluetoothDeviceAdapter.java
+++ app/src/main/java/com/reply/hackaton/biotech/chipitsafe/BluetoothDeviceAdapter.java
@@ -1,18 +1,13 @@
 package com.reply.hackaton.biotech.chipitsafe;
 
 import android.bluetooth.BluetoothDevice;
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.List;
-import java.util.zip.Inflater;
 
 public class BluetoothDeviceAdapter extends RecyclerView.Adapter<DeviceViewHolder>{
 
@@ -60,33 +55,3 @@ class DeviceViewHolder extends RecyclerView.ViewHolder {
     }
 }
 
-/*public class BluetoothDeviceAdapter extends ArrayAdapter<String> {
-
-    public BluetoothDeviceAdapter(Context c, List<String> devices){
-        super(c, 0, devices);
-
-    }
-
-    // called once per list item
-    @NonNull
-    @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) getContext()
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if (inflater != null) {
-            convertView = inflater.inflate(R.layout.row, parent);
-            TextView tv = (TextView) convertView.findViewById(R.id.textViewList);
-            tv.setText(getItem(position));
-        }
-        return convertView;
-    }
-
-    void update(){
-        notifyDataSetInvalidated();
-    }
-
-    void addElement(BluetoothDevice dev){
-        this.add(dev.getName() + " " + dev.getAddress());
-        update();
-    }
-}*/
