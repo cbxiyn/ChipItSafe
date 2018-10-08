761a4ec0e6310aa017af161942d3ab248ef75a2a
Christopher Bailey <c.bailey@reply.eu>
2018-10-08 15:42:43 +0100

    Working implementation of create user config  cloud function.

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -1,36 +1,18 @@
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 //Application Name for email
-const APP_NAME = 'Chip-It-Safe: ECG First Aid';
+const APP_NAME = "Chip-It-Safe: ECG First Aid";
+
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-
-
 admin.initializeApp();
 
 var db = admin.firestore();
-//https://firebase.google.com/docs/reference/functions/functions.auth.UserRecord
-
 
 /**
- * Trigger definitions
+ * Trigger declarations
  */
-/*exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
-  // [END onCreateTrigger]
-    // [START eventAttributes]
-    const email = user.email; // The email of the user.
-    const displayName = user.displayName; // The display name of the user.
-    // [END eventAttributes]
-  
-    return sendWelcomeEmail(email, displayName);
-  });*/
 
 exports.createUserConfig = functions.auth.user().onCreate(user => {
   // [END onCreateTrigger]
@@ -41,31 +23,16 @@ exports.createUserConfig = functions.auth.user().onCreate(user => {
     displayName: user.displayName,
     phoneNumber: user.phoneNumber,
     emailVerified: user.emailVerified
-  }
+  };
   // [END eventAttributes]
-
-  createUserConfig(data);
+  return createUserConfig(data);
 });
 
 /**
  * Create user configuration for user in user config DB
  */
 function createUserConfig(data) {
-    var setDoc =  db.collection('users').doc(data.uid).set(data);
+   db.collection("users").doc(data.uid).set(data);
+   return console.log("User config created");
 }
-/**
- * Sends a welcome email to new user.
- 
-async function sendWelcomeEmail(email, displayName) {
-  const mailOptions = {
-    from: `${APP_NAME} <noreply@firebase.com>`,
-    to: email
-  };
 
-  // The user subscribed to the newsletter.
-  mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName ||
-    ""}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-  await mailTransport.sendMail(mailOptions);
-  return console.log("New welcome email sent to:", email);
-}*/
