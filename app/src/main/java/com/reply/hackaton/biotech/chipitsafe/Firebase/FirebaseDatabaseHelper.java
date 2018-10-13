49d0cc22c5e3675d923bd71fdc35c45291cf8a9a
Christopher Bailey <c.bailey@reply.eu>
2018-10-13 23:55:44 +0200

    Clean Imports

--- app/src/main/java/com/reply/hackaton/biotech/chipitsafe/Firebase/FirebaseDatabaseHelper.java
+++ app/src/main/java/com/reply/hackaton/biotech/chipitsafe/Firebase/FirebaseDatabaseHelper.java
@@ -1,26 +1,17 @@
 package com.reply.hackaton.biotech.chipitsafe.Firebase;
 
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.api.core.ApiFuture;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
-
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /** This class will contain the methods that interact with the Firebase database
  */
