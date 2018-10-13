6e47f005964a9ae3edf4cb875da1d0f243d5099c
Chris Bailey <up757936@myport.ac.uk>
2018-10-13 23:55:44 +0200

    Clean Imports

--- app/src/main/java/com/reply/hackaton/biotech/chipitsafe/Firebase/FirebaseDatabaseHelper.java
+++ app/src/main/java/com/reply/hackaton/biotech/chipitsafe/Firebase/FirebaseDatabaseHelper.java
@@ -1,26 +1,17 @@
 package com.reply.hackaton.biotech.chipitsafe.Firebase;
 
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.api.core.ApiFuture;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
-
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /** This class will contain the methods that interact with the Firebase database
  */
@@ -87,7 +78,6 @@ public class FirebaseDatabaseHelper {
         DocumentReference docRef = db.collection("firstAid").document(uid);
         Log.d(TAG,"Deleting first aid document for user id:" + uid);
         docRef.delete();
-
     }
 
 }
