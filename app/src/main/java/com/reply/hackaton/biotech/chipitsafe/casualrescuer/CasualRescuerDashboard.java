3f0006519e53bce1dcf900fd2661e9c29875eda3
Mario Salierno <m.salierno@reply.it>
2018-10-14 03:52:42 +0200

    rescuer view

--- app/src/main/java/com/reply/hackaton/biotech/chipitsafe/casualrescuer/CasualRescuerDashboard.java
+++ app/src/main/java/com/reply/hackaton/biotech/chipitsafe/casualrescuer/CasualRescuerDashboard.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,7 +27,7 @@ public class CasualRescuerDashboard extends AppCompatActivity implements OnMapRe
     GoogleMap googleMap;
     private double lat;
     private double lon;
-    private String personToBeRescuedName;
+    private String personToBeRescuedName= "hardcoded string";
     private MapFragment mapFragment;
 
     @Override
@@ -34,72 +35,11 @@ public class CasualRescuerDashboard extends AppCompatActivity implements OnMapRe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_casual_rescuer_dashboard);
 
+        TextView tv = findViewById(R.id.)
 
-        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        final Activity ctx = this;
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mapFragment.getMapAsync(new OnMapReadyCallback() {
-                    @Override
-                    public void onMapReady(GoogleMap googleMap) {
-                        GeoLocalizer.openMapsToTheRescueLocation(ctx, SimulationConstants.hLat,SimulationConstants.hLong);
-
-                        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-
-                        googleMap.addMarker(new MarkerOptions()
-                                .position(new LatLng(37.4233438, -122.0728817))
-                                .title("LinkedIn")
-                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-
-                        googleMap.addMarker(new MarkerOptions()
-                                .position(new LatLng(37.4629101,-122.2449094))
-                                .title("Facebook")
-                                .snippet("Facebook HQ: Menlo Park"));
-
-                        googleMap.addMarker(new MarkerOptions()
-                                .position(new LatLng(37.3092293, -122.1136845))
-                                .title("Apple"));
-
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.4233438, -122.0728817), 10));
-
-                    }
-                });
-            }
-        });
-
-        Intent intent = getIntent();
         personToBeRescuedName = intent.getStringExtra("person");
         lat = SimulationConstants.hLat;//intent.getDoubleExtra("lat", SimulationConstants.hLat);
         lon = SimulationConstants.hLong;//intent.getDoubleExtra("long", SimulationConstants.hLong);
     }
 
-
-    @Override
-    public void onMapReady(GoogleMap gMap) {
-        googleMap = gMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        try {
-            googleMap.setMyLocationEnabled(true);
-        } catch (SecurityException se) {
-
-        }
-
-        //Edit the following as per you needs
-        googleMap.setTrafficEnabled(true);
-        googleMap.setIndoorEnabled(true);
-        googleMap.setBuildingsEnabled(true);
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-        //
-
-        LatLng placeLocation = new LatLng(lat, lon); //Make them global
-        Marker placeMarker = googleMap.addMarker(new MarkerOptions().position(placeLocation)
-                .title(personToBeRescuedName));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(placeLocation));
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 1000, null);
-    }
 }
